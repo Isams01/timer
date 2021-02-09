de29add6dfd97d6d3bfdc02ce2443466b2c80a37
Isamu Ito <isamsito@gmail.com>
2021-02-09 19:58:43 +0000

    add edge cases

--- timer1.js
+++ timer1.js
@@ -5,9 +5,16 @@ const beepAt = (time) => {
   }, time);
 };
 
+// No numbers are provided: Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
+// An input is a negative number: Ignore/skip any numbers that are negative. We can't schedule anything in the past.
+// An input is not a number: Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
+
+
 const input = process.argv.slice(2);
 input.forEach(time => {
-  beepAt(parseInt(time) * 1000);
+  if(time > 0 && Number.isInteger(parseFloat(time))){
+    beepAt(parseInt(time) * 1000);
+  }
 });
 
 
