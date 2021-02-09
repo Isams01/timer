9a9bf6411bd4414a1e1a623e05b0279af904be9d
Isamu Ito <isamsito@gmail.com>
2021-02-09 19:59:12 +0000

    remove comments

--- timer1.js
+++ timer1.js
@@ -5,11 +5,6 @@ const beepAt = (time) => {
   }, time);
 };
 
-// No numbers are provided: Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
-// An input is a negative number: Ignore/skip any numbers that are negative. We can't schedule anything in the past.
-// An input is not a number: Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
-
-
 const input = process.argv.slice(2);
 input.forEach(time => {
   if(time > 0 && Number.isInteger(parseFloat(time))){
